67f1fa1cebe945686fb70f7113f342c7ce6c5d09
pmd <info@powermoves.io>
2024-08-28 22:24:28 -0500

    update host value in login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,5 @@
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
-import { cookies, headers } from "next/headers";
+import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 import { Database } from "../../types/supabase";
 import { Login } from "./components/Login";
@@ -21,8 +21,7 @@ export default async function LoginPage({
     redirect("/");
   }
 
-  const headersList = headers();
-  const host = headersList.get("host");
+  const host = 'creatives-generation-ui-hmdvenhfbxepevd8.eastus-01.azurewebsites.net'
 
   return (
     <div className="flex flex-col flex-1 w-full h-[calc(100vh-73px)]">
